d5739570cc2f3cae29b548852d788a4274a156d7
Erik Tollerud <erik.tollerud@gmail.com>
2016-05-09 00:04:01 -0700

    working 1dish

--- minimal_to_wrap.c
+++ minimal_to_wrap.c
@@ -1,9 +1,7 @@
 
-void mul_2d(double ** input_data, double ** result, double mul, int imx, int imy)
+void mul_2d(double * input_data, double * result, double mul, int imx, int imy)
 {
     for ( int i=0; i<imx; i++ ) {    
-    	for ( int j=0; j<imy; j++ ) {
-        	result[i][j] = input_data[i][j] * mul;
-    	}
+        	result[i] = input_data[i] * mul;
     }
 }
